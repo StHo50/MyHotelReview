c1d0dc5520a0c32ed0fea02f33fc5a4dc70e640c
steve <steveholof5072@gmail.com>
2024-09-07 22:48:38 +0300

    Added the Logout button after the users signs in

--- app/src/main/java/com/example/myhotelreview/view/MainActivity.kt
+++ app/src/main/java/com/example/myhotelreview/view/MainActivity.kt
@@ -1,25 +1,36 @@
 package com.example.myhotelreview.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import com.example.myhotelreview.R
 import com.example.myhotelreview.viewmodel.MainViewModel
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     private val mainViewModel: MainViewModel by viewModels()
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val tvWelcome = findViewById<TextView>(R.id.tvWelcome)
+        val btnLogout = findViewById<Button>(R.id.btnLogout)
 
         mainViewModel.welcomeMessage.observe(this, Observer { message ->
             tvWelcome.text = message
         })
+
+        btnLogout.setOnClickListener {
+            auth.signOut()
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
